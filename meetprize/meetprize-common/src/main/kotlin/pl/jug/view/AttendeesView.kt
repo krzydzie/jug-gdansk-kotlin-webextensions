b11b6f9da896be7bb027f5eaefe9a96cceb525f1
Krzysztof Dziekiewicz <krzydzie@wp.pl>
2019-02-27 22:05:27 +0100

    porzadki

--- meetprize/meetprize-common/src/main/kotlin/pl/jug/view/AttendeesView.kt
+++ meetprize/meetprize-common/src/main/kotlin/pl/jug/view/AttendeesView.kt
@@ -4,6 +4,5 @@ import pl.jug.lib.RenderedView
 import pl.jug.model.Attendee
 
 abstract class AttendeesView : RenderedView() {
-    abstract fun showMessage(text: String)
     abstract fun getAttendees(): List<Attendee>
 }
--- meetprize/meetprize-js/src/main/kotlin/pl/jug/view/impl/AttendeesViewImpl.kt
+++ meetprize/meetprize-js/src/main/kotlin/pl/jug/view/impl/AttendeesViewImpl.kt
@@ -4,45 +4,15 @@ import pl.jug.lib.jxQuery
 import pl.jug.model.Attendee
 import pl.jug.view.AttendeesView
 import pl.treksoft.jquery.jQuery
-import webextensions.browser
 
 class AttendeesViewImpl : AttendeesView() {
     override fun render() {
-        console.log("=== AttendeesViewImpl")
-//        ContentPage()
-    }
-
-    override fun showMessage(text: String) {
-        console.log("AttendeesViewImpl text: $text")
-        showAttendee()
-    }
-
-    fun showAttendee() {
-        try {
-            jxQuery("li.attendee-item.list-item").each { _, element ->
-                val meetupProfile = jQuery(selector = "a", context = element).first().attr("href")
-                val name =
-                    jxQuery("span.avatar--person[role='img']", element).first().attr("aria-label")
-                val img = jxQuery("img.avatar-print", element).first().attr("src")
-                console.log(" - profile = $meetupProfile")
-                console.log(" - name = $name")
-                console.log(" - img = $img")
-            }
-        } catch (e: dynamic) {
-            console.error("jest blad")
-            console.error(e)
-            console.dir(e)
-            throw e
-        }
     }
 
     override fun getAttendees(): List<Attendee> {
         val result = mutableListOf<Attendee>()
 
         try {
-            /**
-             * todo zrobic ladniej wyciaganie uczestnikow
-             */
             jxQuery("li.attendee-item.list-item").each { _, element ->
                 val meetupProfile = jQuery(selector = "a", context = element).first().attr("href")
                 val name =
